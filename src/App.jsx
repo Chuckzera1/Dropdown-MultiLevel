781aea4588e6937b735a6cb914401ccf004023a5
Chuck <matheusandreoni@yahoo.com>
2021-06-25 14:09:08 -0300

    MultiLevel Dropdown menu

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+import { useEffect, useRef, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+
+function App() {
+  return (
+    <Navbar>
+      <NavItem icon={<PlusIcon />}/>
+      <NavItem icon={<BellIcon />}/>
+      <NavItem icon={<MessengerIcon />}/>
+
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
+    </Navbar>
+  );
+}
+
+function Navbar({ children }) {
+  return(
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        {children}
+      </ul>
+    </nav>
+  )
+}
+
+function NavItem({ icon, children }) {
+  const [open, setOpen] = useState(false);
+
+  return (<li className="nav-item">
+    <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+      {icon}
+    </a>
+    {open && children}
+  </li>)
+}
+
+function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+  }, [])
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height)
+  }
+
+  function DropdownItem({ children, leftIcon, rightIcon, goToMenu }) {
+    return (
+      <a href="#" className="menu-item" onClick={() => goToMenu && setActiveMenu(goToMenu)} >
+        <span className="icon-button">{leftIcon}</span>
+        {children}
+        <span className="icon-right">{rightIcon}</span>
+      </a>
+    )
+  }
+
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings">
+            Settings
+          </DropdownItem>
+          <DropdownItem
+            leftIcon="🦧"
+            rightIcon={<ChevronIcon />}
+            goToMenu="animals"
+          >
+            Animals
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "settings"}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+            <h2>My Tutorial</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Kangaroo</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Frog</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Horse?</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Hedgehog</DropdownItem>
+        </div>
+      </CSSTransition>
+      
+      <CSSTransition
+        in={activeMenu === "animals"}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+            <h2>Animals</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
+          <DropdownItem leftIcon="🐸">Frog</DropdownItem>
+          <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
+          <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  )
+}
+
+export default App;
